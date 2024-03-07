fdf7512ae7f083bd549feb2d2691fb2343221322
William Coelho <wyllamcoelho8@gmail.com>
2024-03-07 20:57:13 -0300

    Final do modulo 1 iunicio do modulo 2

---
+++ querys.sql
@@ -0,0 +1,24 @@
+-- pesquisar o produto com maior valor 
+
+--SELECT 
+--product_name,
+--price
+--FROM products ORDER BY price DESC LIMIT 5;
+-------------------------------------------------------------------------------------------
+
+-- selecionando produtos especificos com WHERE
+
+--SELECT * 
+--from products 
+--WHERE lower(category) = 'technology'  
+--and (price > 10 OR sub_category = 'Acessories')
+--ORDER BY price LIMIT 15 ;
+-------------------------------------------------------------------------------------------
+
+ -- descobrir os 10 produtos mais caros e com o valor de no maximo 1000 reais
+
+--SELECT *
+--FROM products 
+--WHERE price <= 1000 
+--ORDER BY price DESC LIMIT 10;
+
