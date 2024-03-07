ef62df76be3b1b4f35be080cc0f357739f6dd313
William Coelho <wyllamcoelho8@gmail.com>
2024-03-07 20:17:25 -0300

    ALterei a tabel apara o row_id na ordem!

---
+++ Delete_sales.sql
@@ -0,0 +1,3 @@
+-- DELETE FROM products;
+
+DELETE FROM sales WHERE row_id = 0;
---
+++ Insert_sales.sql
@@ -0,0 +1,2 @@
+INSERT INTO sales(row_id,order_id,order_date,ship_date,ship_mode,customer_id,product_id) 
+VALUES (1,'CA-2017-152156','2022-02-06','2022-02-09','Second Class','CG-12520','FUR-BO-10001798')
