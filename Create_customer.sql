6fb2c21322d73a8892ebad7866fc2dbd8bd17925
William Coelho <wyllamcoelho8@gmail.com>
2024-03-07 18:42:45 -0300

    Criando uma tabela no abanco

---
+++ Create_customer.sql
@@ -0,0 +1,10 @@
+CREATE TABLE customer (
+    customer_id VARCHAR(40),
+    customer_name VARCHAR(50),
+    segment VARCHAR(30),
+    country VARCHAR(30),
+    city VARCHAR(30),
+    state VARCHAR(30),
+    postal_code INT,
+    region VARCHAR(10)
+);
